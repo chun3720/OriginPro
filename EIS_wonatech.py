9fedb1d7a52923de869199f66be7b7cf70a48336
chun3720 <chun3720@hotmail.com>
2022-10-17 16:37:45 +0900

    Update EIS_wonatech.py

--- EIS_wonatech.py
+++ EIS_wonatech.py
@@ -8,17 +8,17 @@ import originpro as op
 # plt.style.use('science')
 year_path  = "D:\\Researcher\\JYCheon\\DATA\\Electrochemistry\\EIS"
 
-raw, path, _, _ = fileloads(year_path, ".xlsx")
+raw, path, _, _ = fileloads(year_path, "csv")
 
-if "raw_split" in os.listdir(path):
-    output_path = f'{path}\\raw_split\\output\\'
-    
-else:
-    output_path = path
+#if "raw_split" in os.listdir(path):
+    #output_path = f'{path}\\raw_split\\output\\'
+    #
+#else:
+    #output_path = path
 #exp_obj = build_data(path, raw, Supercap)
 
 #target_path = path + 'Capacitance\\output\\'
-#output_path = f'{path}\\output\\'
+output_path = f'{path}\\output\\'
 df = pd.read_excel(f'{output_path}EIS_total.xlsx')
 wks = op.find_sheet()
 wks.from_df(df)
